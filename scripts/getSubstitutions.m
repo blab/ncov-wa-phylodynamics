d2fb1f031b926e754da4ca2857e2c59798f41518
Nicola Mueller <nicola.felix.mueller@gmail.com>
2020-11-29 19:12:05 -0800

    updates syns and muts

--- scripts/getSubstitutions.m
+++ scripts/getSubstitutions.m
@@ -3,9 +3,14 @@ clear
 % read in the sequence data
 fasta = fastaread('../../ncov/results/wa_state/sample-division.fasta');
 % 
-fasta_ref = fastaread('../../ncov/results/wa_state/sample-region.fasta');
-reference = fasta_ref(1).Sequence;
+fasta_ref = fastaread('../../ncov/results/wa_state/sample-global.fasta');
+for i=1:length(fasta_ref)
+    if contains(fasta_ref(i).Header, 'Wuhan/WH01/2019')
+        reference = fasta_ref(i).Sequence;
+    end    
+end
 gb_ref = genbankread('../../ncov/defaults/reference_seq.gb');
+
 
 
 f = fopen('../data/combined_meta.tsv');
@@ -23,15 +28,27 @@ end
 fclose(f);
 
 %
-%
 for j = 1 : length(gb_ref.CDS)
     protein{j} = nt2aa(reference(gb_ref.CDS(j).indices(1):gb_ref.CDS(j).indices(2)), 'ACGTOnly', false);
 end
 
 f = fopen('../results/subs_and_muts.tsv', 'w');
-fprintf(f, 'Sequence\tDate\tAA\tMutations\n');
+fprintf(f, 'Sequence\tDate');
+for j = 1 : length(gb_ref.CDS)
+    fprintf(f, '\t%s', gb_ref.CDS(j).gene);
+end
+fprintf(f, '\tAA\tMutations\n');
 
 for i = 1 : length(fasta)
+    ind = find(ismember(id,fasta(i).Header));
+    if isempty(ind)
+        date_str = 'NA';
+    else
+        date_str = date{ind};
+    end
+
+    fprintf(f,'%s\t%s', fasta(i).Header,date_str);
+
     AA = 0;
     % compare AA
     fasta(i).Sequence(fasta(i).Sequence=='-')='N';
@@ -40,19 +57,14 @@ for i = 1 : length(fasta)
         diffs = protein{j}~=AA_vals;
         diffs(AA_vals=='X')=0;
         diffs(protein{j}=='X')=0;
+        fprintf(f, '\t%d', sum(diffs));
         AA=AA+sum(diffs);
     end
     % compare RNA
     diffs = reference~=fasta(i).Sequence;
     diffs(reference=='N') = 0;
     diffs(fasta(i).Sequence=='N') = 0;
-    ind = find(ismember(id,fasta(i).Header));
-    if isempty(ind)
-        date_str = 'NA';
-    else
-        date_str = date{ind};
-    end
-    fprintf(f,'%s\t%s\t%d\t%d\n', fasta(i).Header,date_str, AA, sum(diffs));
+    fprintf(f,'\t%d\t%d\n', AA, sum(diffs));
 end
 fclose(f);
 
